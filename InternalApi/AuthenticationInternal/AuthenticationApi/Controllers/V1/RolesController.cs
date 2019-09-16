67d87adc068f2f6e1a7562faf6e6245a80fc8df2
adriannetubig@gmail.com <adriannetubig@gmail.com>
2019-09-16 16:46:52 +0800

    Roles List

---
+++ InternalApi/AuthenticationInternal/AuthenticationApi/Controllers/V1/RolesController.cs
@@ -0,0 +1,26 @@
+﻿using AuthenticationBusiness.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AuthenticationApi.Controllers.V1
+{
+    public class RolesController : BaseControllerV1
+    {
+        private readonly IBusinessRoles _iBusinessRoles;
+
+        public RolesController(IBusinessRoles iBusinessRoles)
+        {
+            _iBusinessRoles = iBusinessRoles;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Read(CancellationToken cancellationToken)
+        {
+            var requestResult = await _iBusinessRoles.Read(cancellationToken);
+
+            return Ok(requestResult);
+        }
+
+    }
+}
