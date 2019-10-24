6f7f7aba4f4d0324e6487576dc498c61e54ca0b1
adriannetubig@gmail.com <adriannetubig@gmail.com>
2019-10-24 18:08:49 +0800

    Finished loading of paged exception logs

---
+++ InternalApi/ErrorLoggerInternal/ErrorLoggerBusiness/Interfaces/IBusinessExceptionLogs.cs
@@ -0,0 +1,13 @@
+﻿using BaseModel;
+using ErrorLoggerBusiness.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ErrorLoggerBusiness.Interfaces
+{
+    public interface IBusinessExceptionLogs
+    {
+        Task<RequestResult<ExceptionLog>> Create(CancellationToken cancellationToken, ExceptionLog exceptionLog, string applicationName);
+        Task<RequestResult<PagedList<ExceptionLog>>> Read(CancellationToken cancellationToken, bool ascending, int itemsPerPage, int pageNo, string sortBy);
+    }
+}
--- InternalApi/ErrorLoggerInternal/ErrorLoggerBusiness/Services/BusinessExceptionLogs.cs
+++ InternalApi/ErrorLoggerInternal/ErrorLoggerBusiness/Services/BusinessExceptionLogs.cs
@@ -4,8 +4,6 @@ using BaseModel;
 using ErrorLoggerBusiness.Models;
 using ErrorLoggerData.Entities;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -34,6 +32,17 @@ namespace ErrorLoggerBusiness.Services
                 entityExceptionLog.InnerExceptionLog.CreatedDateUtc = DateTime.UtcNow;
 
             await _iRepoBase.Create(entityExceptionLog, cancellationToken);
+
+            return requestResult;
+        }
+
+        public async Task<RequestResult<PagedList<ExceptionLog>>> Read(CancellationToken cancellationToken, bool ascending, int itemsPerPage, int pageNo, string sortBy)
+        {
+            var requestResult = new RequestResult<PagedList<ExceptionLog>>();
+
+            var entityPagedList = await _iRepoBase.ReadMultiple<ExceptionLog>(a => true, ascending, itemsPerPage, pageNo, sortBy, cancellationToken, a => a.InnerExceptionLog);
+
+            requestResult.Model = _iMapper.Map<PagedList<ExceptionLog>>(entityPagedList);
 
             return requestResult;
         }
