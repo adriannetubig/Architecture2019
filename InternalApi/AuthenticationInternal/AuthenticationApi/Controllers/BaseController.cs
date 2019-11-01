75ba08c7e8b84b9f73e6f9858184d690e8b62acb
adriannetubig@gmail.com <adriannetubig@gmail.com>
2019-11-01 08:25:27 +0800

    Enabled Versioning

--- InternalApi/AuthenticationInternal/AuthenticationApi/Controllers/BaseController.cs
+++ InternalApi/AuthenticationInternal/AuthenticationApi/Controllers/BaseController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace AuthenticationApi.Controllers
 {
     [EnableCors("CORS")]
+    [Route("api/v{v:apiVersion}/[controller]")]
     [Authorize]
     public abstract class BaseController : BaseApiController
     {
--- InternalApi/AuthenticationInternal/AuthenticationApi/Controllers/V1/BaseControllerV1.cs
+++ InternalApi/AuthenticationInternal/AuthenticationApi/Controllers/V1/BaseControllerV1.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace AuthenticationApi.Controllers.V1
+﻿namespace AuthenticationApi.Controllers.V1
 {
-    [Route("api/v1/[controller]")]
     public abstract class BaseControllerV1 : BaseController
     {
     }
